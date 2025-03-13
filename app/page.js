093574f364ec66b954b8d1eb9c38b91f3731af7b
Jan Luan <jan.luan@mail.utoronto.ca>
2025-03-13 00:26:31 +0000

    submit

--- app/page.js
+++ app/page.js
@@ -1,28 +1,49 @@
+// Name: Jan Luan
+// UTorID: luanjan
 // app/page.js
 export default function Home() {
-  return (
-    <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
-      <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
-        <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
-        <div className="left-[50px] top-0 absolute justify-start text-[#db4439] text-base font-black font-['Inknut_Antiqua']">Click</div>
-      </div>
-      <div className="origin-top-left rotate-[-8.64deg] w-[543px] h-[590.40px] left-[1068.69px] top-[226px] absolute">
-        <div className="w-[543px] h-[590.40px] left-[88.69px] top-0 absolute bg-[#d9d9d9]" />
-        <img className="w-[630.99px] h-[1054.71px] left-[81.51px] top-[-57.62px] absolute" src="https://placehold.co/631x1055" />
-      </div>
-      <div className="w-[680px] left-[143px] top-[313px] absolute justify-start text-[#eaecd7] text-[110px] font-black font-['Inknut_Antiqua'] leading-[134.20px]">This is the Topic This is the Topic</div>
-      <div className="w-[680px] left-[148px] top-[740px] absolute justify-start text-[#eaecd7] text-[40px] font-semibold font-['Inknut_Antiqua'] leading-[48.80px]">This is the Topic </div>
-      <div className="left-[270px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Me</div>
-      <div className="left-[64px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="left-[891px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[965px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[716px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="w-[1804px] h-[102px] left-[-35px] top-[1049px] absolute bg-white" />
-      <div className="left-[124px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[459px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[794px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1464px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1129px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-    </div>
-  );
-}
+    return (
+        <div className="w-full h-screen flex flex-col bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
+            <nav className="flex justify-between items-center p-6 text-2xl font-bold text-[#eaecd7] font-['Inknut_Antiqua']">
+                <div className="flex space-x-8">
+                    <span>Home</span>
+                    <span>Coffee</span>
+                    <span>Bagels</span>
+                    <span>Tea</span>
+                    <span>Sandwich</span>
+                </div>
+            </nav>
+
+            <div className="absolute top-6 right-10 w-[150px] h-[52px] bg-[#eaecd7] rounded-full flex items-center justify-center text-[#db4439] text-base font-black font-['Inknut_Antiqua']">
+                Login
+            </div>
+
+            <div className="flex flex-grow items-center justify-center px-8 gap-12">
+                <div className="text-left max-w-xl">
+                    <h1 className="text-[#eaecd7] text-[80px] font-black font-['Inknut_Antiqua'] leading-[100px]">
+                        Welcome
+                    </h1>
+                    <p className="text-[#eaecd7] text-[30px] font-semibold font-['Inknut_Antiqua'] leading-[40px] mt-4">
+                        The best coffee shop
+                    </p>
+                </div>
+
+                <div className="relative w-[400px] h-[400px]">
+                    <img
+                        className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 w-[420px] h-[450px]"
+                        src="https://images.pexels.com/photos/1170659/pexels-photo-1170659.jpeg?cs=srgb&dl=art-bread-brown-1170659.jpg&fm=jpg"
+                        alt="Placeholder"
+                    />
+                </div>
+            </div>
+
+            <footer className="w-full h-[80px] bg-white flex justify-around items-center text-[#db4439] text-xl font-black font-['Inknut_Antiqua']">
+                <span>Coffee</span>
+                <span>Bagels</span>
+                <span>Tea</span>
+                <span>Contact me</span>
+                <span>Privacy policy</span>
+            </footer>
+        </div>
+    );
+            }
